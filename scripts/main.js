4aef4e0df4c978742978e34f030f2b4a680d03f4
Jing Ran <jingran.mavis@gmail.com>
2017-06-25 22:22:32 -0400

    Fix calcRightPieces function and clean up files

--- scripts/main.js
+++ scripts/main.js
@@ -14,14 +14,12 @@
   var gameTimeEasy = 8 * 60 * 1000;
   var gameTimeHard = 4 * 60 * 1000;
   var gameTime = gameTimeEasy;
-  // var gameTime = 0.1*60*1000;
   var continueBtn = document.querySelector('.c-btn--continue');
   var restartBtnPause = document.querySelector('.c-modal--pause .c-btn--restart');
   var pauseModal = document.querySelector('.c-modal--pause');
   var overlay = document.querySelector('.c-modal-overlay');
   var restartBtnGameover = document.querySelector('.c-modal--gameover .c-btn--restart');
   var gameoverModal = document.querySelector('.c-modal--gameover');
-  var rightPieces = 0;
   var puzzlePieceArr = ['0-0', '0-1', '0-2', '0-3', '0-4', '1-0', '1-1', '1-2',
     '1-3', '2-0', '2-1', '2-2', '2-3', '2-4', '3-0', '3-1', '3-2', '3-3',
     '4-0', '4-1', '4-2', '4-3', '4-4', '5-0', '5-1', '5-2', '5-3', '6-0',
@@ -29,10 +27,9 @@
     '8-2', '8-3', '8-4'];
   var piecesStorage = document.querySelector('.l-pieces-storage');
   var dropZone = document.querySelector('.c-pieces--drop-zone');
-
   var restartBtnCong = document.querySelector('.c-modal--congrats .c-btn--restart');
   var congratsModal = document.querySelector('.c-modal--congrats');
-
+  var rightPieces = 0;
   var startZoneId;
 
 
@@ -40,7 +37,6 @@
   // Countdown object
   var countdownObj = {
     start: function(endTime) {
-      // console.log('start endTime ' + Date.parse(endTime));
       var self = this;
       var timeLeft = Date.parse(endTime) - Date.parse(new Date());
       var seconds = Math.floor((timeLeft/1000) % 60);
@@ -54,24 +50,19 @@
       if (timeLeft <= 60000) countdownTimer.style.color = 'red';
 
       if (timeLeft <= 0) {
-        console.log('game over');
         clearTimeout(this.timeout);
         delete this.timeout;
-
         if (rightPieces !== 41) gameOver();
-
         return false;
       }
 
       this.timeout = window.setTimeout(function() {
         self.gameTimeLeft = timeLeft;
-        // console.log('self.gameTL: ' + self.gameTimeLeft);
         self.start(endTime);
       }, 1000);
     },
 
     pause: function() {
-      console.log('pause');
       clearTimeout(this.timeout);
       delete this.timeout;
     },
@@ -130,7 +121,6 @@
   }
   
   function hideHint() {
-    // hide hint
     hintImg.style.visibility = 'hidden';
     if (hintCount === 0) {
       hintBtn.disabled = true;
@@ -145,10 +135,8 @@
   }
 
   function congrats() {
-    console.log('congrats func');
     var allPieces = document.querySelectorAll('.c-pieces__piece-img');
     for (var i = 0; i < allPieces.length; i++) {
-      console.log('congrats loop');
       allPieces[i].classList.add('congrats-flip');
     }
   }
@@ -181,41 +169,35 @@
 
 
   function handleDragStart(e) {
-    console.log('drag start ' + e.target.id);
     if (e.target.classList.contains('c-pieces__piece-img')) {
+      var startLocation = e.target.parentNode;
       e.dataTransfer.setData('text/plain', e.target.id);
 
-      var startLocation = e.target.parentNode;
-
       if (startLocation.classList.contains('c-pieces__piece'))
+        // if move a piece from one drop span to another
+        // startZone indicates the previous drop span
         startZoneId = startLocation.id.substr(-3);
     }
-
   }
 
   function handleDragOver(e) {
-    console.log('drag over ');
     // Allow drop
     e.preventDefault();
   }
 
   function handleDrop(e) {
-    // e.preventDefault();
-    // e.stopPropagation();
-    console.log('Drop ');
+    e.preventDefault();
+    e.stopPropagation();
     // Get the data, which is the id of the DragStart target
     var targetId = e.dataTransfer.getData('text');
 
     if (e.target.classList.contains('c-pieces__piece')) { // dropSpan is empty
-      console.log('drop if' + targetId);
       e.target.appendChild(document.getElementById(targetId));
 
       // call calcRightPieces func
       calcRightPieces(e.target, targetId);
-
     } else if (e.target.classList.contains('c-pieces__piece-img') &&
-      targetId !== e.target.id) { // dropSpan not empty
-      console.log('drop else if ' + e.target.id, e.dataTransfer.getData('text'));
+      targetId !== e.target.id)  { // dropSpan not empty
       swapPieces(targetId, e.target);
     }
 
@@ -223,16 +205,14 @@
     e.dataTransfer.clearData();
   }
 
-  // call this func when dropping a piece
+
   function swapPieces(newPieceId, oldPiece) {
-    console.log('swap');
     var newPiece = document.getElementById(newPieceId);
     var dropSpan = oldPiece.parentNode;
     dropSpan.replaceChild(newPiece, oldPiece);
     piecesStorage.appendChild(oldPiece);
 
-    // call calcRightPieces func
-    console.log('old piece id ' + oldPiece.id);
+    // recalculate number of right pieces
     calcRightPieces(dropSpan, newPieceId, oldPiece.id);
   }
 
@@ -240,43 +220,34 @@
   // call this func when a piece is dropped
   function calcRightPieces(dropSpan, currentPieceId, prevPieceId) {
     var dropSpanId = dropSpan.id.substr(-3);
-    console.log('rightorwrong ' + dropSpanId, currentPieceId, prevPieceId);
-
-    // if (dropSpan.children.length === 0) { // no piece in this dropSpan before
-    //   if (dropSpanId === currentPieceId) rightPieces++;
-    // } else { // called by swapPieces func
-    //   if (prevPieceId === dropSpanId) {
-    //     rightPieces--; // prevPiece is right, then current must be wrong
-    //   } else {
-    //     if (dropSpanId === currentPieceId) rightPieces++;
-    //   }
-    // }
-
 
     if (dropSpanId === currentPieceId) {
-      console.log('if right++ ');
       rightPieces++;
-    }
-    else {
-      console.log('else ' + startZoneId);
-      if (prevPieceId === dropSpanId || currentPieceId === startZoneId) {
-        console.log('else right-- ');
+    } else {
+      // if previous piece was in the right span -- current piece is wrong
+      // OR if current piece was right and is moving to a wrong span
+      if (prevPieceId === dropSpanId || currentPieceId === startZoneId)
         rightPieces--;
-      }
+
+      // if both previous and current were in right spans, and they are exchanged
+      if (currentPieceId === startZoneId && prevPieceId === dropSpanId)
+        rightPieces--;
     }
     console.log('right number ' + rightPieces);
 
     // pieces === 41 && nothing in storage area
     if (rightPieces === 41) {
-      console.log('ALL are right');
-      // dropZone.style.pointerEvents = 'none';
-
-      // TODO: add congratulations
-      // TODO: disable all buttons (may achieve thru congrats modal)
       openModal(congratsModal);
       countdownObj.pause();
       congrats();
     }
+  }
+
+  function restartGame(modal) {
+    closeModal(modal);
+    window.location.reload(true);
+    startZoneId = '';
+    rightPieces = 0;
   }
 
 
@@ -287,10 +258,8 @@
 
   gameLevelSwitch.addEventListener('click', changeGameLevel);
 
-  // startBtn.addEventListener('click', startGame);
   startBtn.addEventListener('click', function() {
     startGame();
-
     loadPieces();
 
     // start countdown timer
@@ -298,9 +267,7 @@
     countdownObj.start(deadline);
   });
 
-  // show or hide hint img
   hintBtn.addEventListener('mousedown', function() {
-    // show hint
     showHint();
   });
 
@@ -319,22 +286,17 @@
   });
 
   restartBtnPause.addEventListener('click', function () {
-    closeModal(pauseModal);
-    window.location.reload(true);
+    restartGame(pauseModal);
   });
 
   restartBtnGameover.addEventListener('click', function () {
-    closeModal(pauseModal);
-    window.location.reload(true);
+    restartGame(gameoverModal);
   });
 
   restartBtnCong.addEventListener('click', function () {
-    closeModal(pauseModal);
-    window.location.reload(true);
+    restartGame(congratsModal);
   });
 
-
-  dropZone.addEventListener('dragstart', handleDragStart);
   dropZone.addEventListener('dragover', handleDragOver);
   dropZone.addEventListener('drop', handleDrop);
 })();
